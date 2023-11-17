aba0411e7ab3c3721dabf50a401d73043b80178c
kanenorman <kanenorman1996@gmail.com>
2023-11-17 12:14:22 -0500

    Add support for fullpage.js
    Incorporate scrolling through slides

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    // Options
+    sectionsColor: [
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FFFFFF",
+    ],
+    navigation: true,
+    navigationPosition: "right",
+  });
+});
