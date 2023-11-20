72ecbf1311a7a0a17dfb5883cbeb1abed37b83a3
kanenorman <kanenorman1996@gmail.com>
2023-11-20 06:00:05 -0500

    Move breaking news bar
    Persist at bottom on scroll

--- js/main.js
+++ js/main.js
@@ -23,30 +23,3 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Select all sections
-  var sections = document.querySelectorAll(".section");
-
-  // HTML content for the news container
-  var newsContainerHTML = `
-        <div class="news-container">
-            <div class="title">
-                Breaking News
-            </div>
-            <ul>
-                <li>Kane is a goated JS developer!</li>
-                <li>Luke really works for the CIA!</li>
-                <li>Andrew is a delusional Eagles fan. Go Cowboys!</li>
-            </ul>
-        </div>
-    `;
-
-  // Append the news container to each section except #section-1
-  sections.forEach(function (section) {
-    // Check if the section is not #section-1
-    if (section.id !== "section-1") {
-      section.insertAdjacentHTML("beforeend", newsContainerHTML);
-    }
-  });
-});
