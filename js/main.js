74e042c5476a99df4e712ccda63b94d4a60d8117
kanenorman <kanenorman1996@gmail.com>
2023-11-23 17:58:52 -0600

    Apply formatting

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@
 const REPUBLICAN_RED = "#C90A3D";
 const DEMOCRAT_BLUE = "#5768AC";
 const INDEPENDENT_GRAY = "#808080";
+const PARTY_COLOR_MAP = {
+  R: REPUBLICAN_RED,
+  D: DEMOCRAT_BLUE,
+  I: INDEPENDENT_GRAY,
+};
 
 // Set default ease
 TweenMax.defaultEase = Linear.easeOut;
