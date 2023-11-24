481d6098cdb28d7f6ecf85cc853a2a614eb6b720
kanenorman <kanenorman1996@gmail.com>
2023-11-23 21:29:39 -0600

    Add slider to by network
    Slider works but needs formatting

--- js/by-network.js
+++ js/by-network.js
@@ -1,165 +1,182 @@
-function loadData() {
-  var slider = document.getElementById("slider");
+class ByNetworkVisual {
+  constructor() {
+    this.loadData();
+  }
 
-  d3.csv("data/labeled.csv", (row) => {
-    row.date = d3.timeParse("%Y%m%d")(row.date);
-    return row;
-  }).then((data) => {
-    let minDate = d3.min(data, (d) => d.date).getTime(); // Convert to timestamp
-    let maxDate = d3.max(data, (d) => d.date).getTime(); // Convert to timestamp
+  async loadData() {
+    const slider = document.getElementById("slider");
+    const data = await d3.csv("data/labeled.csv", (row) => {
+      row.date = d3.timeParse("%Y%m%d")(row.date);
+      return row;
+    });
 
+    this.minDate = d3.min(data, (d) => d.date).getTime();
+    this.maxDate = d3.max(data, (d) => d.date).getTime();
+
+    this.setupSlider(slider, data);
+    this.updateByNetwork(data);
+  }
+
+  setupSlider(slider, data) {
     noUiSlider.create(slider, {
-      start: [minDate, maxDate],
+      start: [this.minDate, this.maxDate],
       connect: true,
       behaviour: "drag",
       step: 1,
       range: {
-        min: minDate,
-        max: maxDate,
+        min: this.minDate,
+        max: this.maxDate,
       },
       tooltips: {
-        to: function (value) {
-          return parseInt(value);
-        },
+        to: (value) => parseInt(value),
       },
     });
 
-    slider.noUiSlider.on("slide", function (values) {
-      let selectedMinYear = new Date(+values[0]);
-      let selectedMaxYear = new Date(+values[1]);
+    slider.noUiSlider.on("slide", (values) => {
+      const selectedMinYear = new Date(+values[0]);
+      const selectedMaxYear = new Date(+values[1]);
 
-      filteredData = data.filter(
+      const filteredData = data.filter(
         (d) => d.date >= selectedMinYear && d.date <= selectedMaxYear
       );
 
-      // Call the updateVisualization function with the filtered data
-      updateByNetwork(filteredData);
+      this.updateByNetwork(filteredData);
     });
+  }
 
-    updateByNetwork(data);
-  });
+  updateByNetwork(data) {
+    const networks = Array.from(new Set(data.map((d) => d.network))).sort(
+      d3.ascending
+    );
+
+    const avgScores = d3
+      .rollups(
+        data,
+        (v) => d3.mean(v, (d) => d.label),
+        (d) => d.network,
+        (d) => d.party
+      )
+      .map(([network, partyData]) =>
+        partyData.map(([party, avgScore]) => ({ network, party, avgScore }))
+      )
+      .flat();
+
+    const democratSentiment = avgScores
+      .filter((d) => d.party === "D")
+      .map((d) => d.avgScore);
+
+    const republicanSentiment = avgScores
+      .filter((d) => d.party === "R")
+      .map((d) => d.avgScore);
+
+    this.createOrUpdateChart(networks, democratSentiment, republicanSentiment);
+  }
+
+  createOrUpdateChart(names, leftData, rightData) {
+    this.labelArea = 160;
+    this.width = 400;
+    this.barHeight = 20;
+    this.height = this.barHeight * names.length;
+    this.rightOffset = this.width + this.labelArea;
+    this.leftPad = 25;
+
+    // Create or select the SVG element
+    this.chart = d3.select("#by-network").select("svg.chart");
+    if (this.chart.empty()) {
+      this.chart = d3
+        .select("#by-network")
+        .append("svg")
+        .attr("class", "chart")
+        .attr("width", this.labelArea + this.width + this.width)
+        .attr("height", this.height);
+    }
+
+    // Scales
+    this.xFrom = d3
+      .scaleLinear()
+      .domain([0, d3.max(leftData)])
+      .range([0, this.width]);
+
+    this.y = d3.scaleBand().domain(names).range([10, this.height]).padding(0.1);
+
+    this.xTo = d3
+      .scaleLinear()
+      .domain([0, d3.max(rightData)])
+      .range([0, this.width]);
+
+    // Update Democrat Chart
+    this.updateDemocrat(leftData, names);
+
+    // Update Y-axis Labels
+    this.updateAxisLabels(names);
+
+    // Update Republican Chart
+    this.updateRepublican(rightData, names);
+  }
+
+  updateRepublican(rightData, names) {
+    this.chart
+      .selectAll("rect.right")
+      .data(rightData)
+      .join("rect")
+      .attr("x", this.rightOffset)
+      .attr("y", (d, i) => this.y(names[i]))
+      .attr("class", "right")
+      .attr("width", this.xTo)
+      .attr("height", this.y.bandwidth())
+      .attr("fill", REPUBLICAN_RED);
+
+    this.chart
+      .selectAll("text.score")
+      .data(rightData)
+      .join("text")
+      .attr("x", (d) => this.xTo(d) + this.rightOffset)
+      .attr("y", (d, i) => this.y(names[i]) + this.y.bandwidth() / 2)
+      .attr("dx", -5)
+      .attr("dy", ".36em")
+      .attr("text-anchor", "end")
+      .attr("class", "score")
+      .attr("fill", "#FFFFFF")
+      .text((d) => `${(d * 100).toFixed(0)}%`);
+  }
+
+  updateDemocrat(leftData, names) {
+    this.chart
+      .selectAll("rect.left")
+      .data(leftData)
+      .join("rect")
+      .attr("x", (d) => this.width - this.xFrom(d))
+      .attr("y", (d, i) => this.y(names[i]))
+      .attr("class", "left")
+      .attr("width", this.xFrom)
+      .attr("height", this.y.bandwidth())
+      .attr("fill", DEMOCRAT_BLUE);
+
+    this.chart
+      .selectAll("text.leftscore")
+      .data(leftData)
+      .join("text")
+      .attr("x", (d) => this.width - this.xFrom(d) + this.leftPad)
+      .attr("y", (d, i) => this.y(names[i]) + this.y.bandwidth() / 2)
+      .attr("dx", "20")
+      .attr("dy", ".36em")
+      .attr("text-anchor", "end")
+      .attr("class", "leftscore")
+      .attr("fill", "#FFFFFF")
+      .text((d) => `${(d * 100).toFixed(0)}%`);
+  }
+  updateAxisLabels(names) {
+    this.chart
+      .selectAll("text.name")
+      .data(names)
+      .join("text")
+      .attr("x", this.labelArea / 2 + this.width)
+      .attr("y", (d) => this.y(d) + this.y.bandwidth() / 2)
+      .attr("dy", ".20em")
+      .attr("text-anchor", "middle")
+      .attr("class", "name")
+      .text(String);
+  }
 }
 
-function updateByNetwork(data) {
-  const networks = Array.from(new Set(data.map((d) => d.network))).sort(
-    d3.ascending
-  );
-
-  const avgScores = d3
-    .rollups(
-      data,
-      (v) => d3.mean(v, (d) => d.label),
-      (d) => d.network,
-      (d) => d.party
-    )
-    .map(([network, partyData]) =>
-      partyData.map(([party, avgScore]) => ({ network, party, avgScore }))
-    )
-    .flat();
-
-  const democratSentiment = avgScores
-    .filter((d) => d.party === "D")
-    .map((d) => d.avgScore);
-
-  const republicanSentiment = avgScores
-    .filter((d) => d.party === "R")
-    .map((d) => d.avgScore);
-
-  var names = networks;
-  var leftData = democratSentiment;
-  var rightData = republicanSentiment;
-
-  // Chart setup
-  var labelArea = 160;
-  var chart,
-    width = 400,
-    bar_height = 20,
-    height = 900;
-  var rightOffset = width + labelArea;
-  var leftPad = 25;
-  var chart = d3
-    .select("#by-network")
-    .append("svg")
-    .attr("class", "chart")
-    .attr("width", labelArea + width + width)
-    .attr("height", 900);
-
-  // Scales
-  var xFrom = d3
-    .scaleLinear()
-    .domain([0, d3.max(leftData)])
-    .range([0, width]);
-
-  var y = d3.scaleBand().domain(names).range([10, height]).padding(0.1);
-
-  var xTo = d3
-    .scaleLinear()
-    .domain([0, d3.max(rightData)])
-    .range([0, width]);
-
-  // Democrat bars
-  chart
-    .selectAll("rect.left")
-    .data(leftData)
-    .join("rect")
-    .attr("x", (d) => width - xFrom(d))
-    .attr("y", (d, i) => y(names[i]))
-    .attr("class", "left")
-    .attr("width", xFrom)
-    .attr("height", y.bandwidth())
-    .attr("fill", DEMOCRAT_BLUE);
-
-  // Democrat labels
-  chart
-    .selectAll("text.leftscore")
-    .data(leftData)
-    .join("text")
-    .attr("x", (d) => width - xFrom(d) + leftPad)
-    .attr("y", (d, i) => y(names[i]) + y.bandwidth() / 2)
-    .attr("dx", "20")
-    .attr("dy", ".36em")
-    .attr("text-anchor", "end")
-    .attr("class", "leftscore")
-    .attr("fill", "#FFFFFF")
-    .text((d) => `${(d * 100).toFixed(0)}%`);
-
-  // Y-axis Labels
-  chart
-    .selectAll("text.name")
-    .data(names)
-    .join("text")
-    .attr("x", labelArea / 2 + width)
-    .attr("y", (d) => y(d) + y.bandwidth() / 2)
-    .attr("dy", ".20em")
-    .attr("text-anchor", "middle")
-    .attr("class", "name")
-    .text(String);
-
-  // Republican Bars
-  chart
-    .selectAll("rect.right")
-    .data(rightData)
-    .join("rect")
-    .attr("x", rightOffset)
-    .attr("y", (d, i) => y(names[i]))
-    .attr("class", "right")
-    .attr("width", xTo)
-    .attr("height", y.bandwidth())
-    .attr("fill", REPUBLICAN_RED);
-
-  // Republican Labels
-  chart
-    .selectAll("text.score")
-    .data(rightData)
-    .join("text")
-    .attr("x", (d) => xTo(d) + rightOffset)
-    .attr("y", (d, i) => y(names[i]) + y.bandwidth() / 2)
-    .attr("dx", -5)
-    .attr("dy", ".36em")
-    .attr("text-anchor", "end")
-    .attr("class", "score")
-    .attr("fill", "#FFFFFF")
-    .text((d) => `${(d * 100).toFixed(0)}%`);
-}
-
-loadData();
+new ByNetworkVisual();
