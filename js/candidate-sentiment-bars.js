e71acc24ceac658e0343a128bc6bd534af425088
kanenorman <kanenorman1996@gmail.com>
2023-11-26 18:50:23 -0600

    Refactor candidate sentiment

--- js/candidate-sentiment-bars.js
+++ js/candidate-sentiment-bars.js
@@ -1,78 +1,106 @@
-function updateChart(data, svg, x, y, height) {
-  // Clear existing chart elements
-  svg.selectAll("*").remove();
+class SentimentChart {
+  constructor(selector, width, height) {
+    this.margin = { top: 30, right: 30, bottom: 30, left: 230 };
+    this.width = width - this.margin.left - this.margin.right;
+    this.height = height - this.margin.top - this.margin.bottom;
+    this.selector = selector;
+    this.initChart();
+  }
 
-  // Re-create X axis
-  svg
-    .append("g")
-    .attr("transform", `translate(0,${height})`)
-    .call(d3.axisBottom(x).ticks(5));
+  initChart() {
+    this.x = d3.scaleLinear().domain([0, 1]).range([0, this.width]);
+    this.y = d3.scaleBand().range([0, this.height]).padding(0.1);
 
-  // Update Y axis domain based on new data
-  y.domain(data.map((d) => d.name));
-  svg
-    .append("g")
-    .call(d3.axisLeft(y).tickSize(0))
-    .selectAll(".tick text")
-    .attr("x", -y.bandwidth() * 1.2)
-    .style("text-anchor", "end")
-    .style("font-size", "16px");
+    this.svg = d3
+      .select(this.selector)
+      .append("svg")
+      .attr("width", this.width + this.margin.left + this.margin.right)
+      .attr("height", this.height + this.margin.top + this.margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${this.margin.left},${this.margin.top})`);
+  }
 
-  // Add bars
-  svg
-    .selectAll("myRect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", x(0))
-    .attr("y", (d) => y(d.name))
-    .attr("width", (d) => x(d.avg_sentiment))
-    .attr("height", y.bandwidth())
-    .attr("fill", (d) => (d.party === "R" ? REPUBLICAN_RED : DEMOCRAT_BLUE));
+  updateChart(data) {
+    // Clear existing chart elements
+    this.svg.selectAll("*").remove();
 
-  // Add bar extensions
-  svg
-    .selectAll("barExtensions")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", x(0) - y.bandwidth() / 2)
-    .attr("y", (d) => y(d.name))
-    .attr("width", y.bandwidth())
-    .attr("height", y.bandwidth())
-    .attr("fill", (d) => (d.party === "R" ? REPUBLICAN_RED : DEMOCRAT_BLUE));
+    // Re-create X axis
+    this.svg
+      .append("g")
+      .attr("transform", `translate(0,${this.height})`)
+      .call(d3.axisBottom(this.x).ticks(5));
 
-  // Add background circles
-  svg
-    .selectAll("backgroundCircles")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", x(0) - y.bandwidth() / 2)
-    .attr("cy", (d) => y(d.name) + y.bandwidth() / 2)
-    .attr("r", y.bandwidth() / 2)
-    .attr("fill", "white")
-    .attr("stroke", (d) => (d.party === "R" ? REPUBLICAN_RED : DEMOCRAT_BLUE))
-    .attr("stroke-width", 3);
+    // Update Y axis domain based on new data
+    this.y.domain(data.map((d) => d.name));
+    this.svg
+      .append("g")
+      .call(d3.axisLeft(this.y).tickSize(0))
+      .selectAll(".tick text")
+      .attr("x", -this.y.bandwidth() * 1.2)
+      .style("text-anchor", "end")
+      .style("font-size", "16px");
 
-  // Add images
-  svg
-    .selectAll("candidateImages")
-    .data(data)
-    .enter()
-    .append("image")
-    .attr("xlink:href", (d) => d.photo)
-    .attr("x", x(0) - y.bandwidth())
-    .attr("y", (d) => y(d.name))
-    .attr("height", y.bandwidth())
-    .attr("width", y.bandwidth())
-    .attr("clip-path", "circle()");
+    // Add bars
+    this.svg
+      .selectAll("myRect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", this.x(0))
+      .attr("y", (d) => this.y(d.name))
+      .attr("width", (d) => this.x(d.avg_sentiment))
+      .attr("height", this.y.bandwidth())
+      .attr("fill", (d) => (d.party === "R" ? REPUBLICAN_RED : DEMOCRAT_BLUE));
+
+    // Add bar extensions
+    this.svg
+      .selectAll("barExtensions")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", this.x(0) - this.y.bandwidth() / 2)
+      .attr("y", (d) => this.y(d.name))
+      .attr("width", this.y.bandwidth())
+      .attr("height", this.y.bandwidth())
+      .attr("fill", (d) => (d.party === "R" ? REPUBLICAN_RED : DEMOCRAT_BLUE));
+
+    // Add background circles
+    this.svg
+      .selectAll("backgroundCircles")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", this.x(0) - this.y.bandwidth() / 2)
+      .attr("cy", (d) => this.y(d.name) + this.y.bandwidth() / 2)
+      .attr("r", this.y.bandwidth() / 2)
+      .attr("fill", "white")
+      .attr("stroke", (d) => (d.party === "R" ? REPUBLICAN_RED : DEMOCRAT_BLUE))
+      .attr("stroke-width", 3);
+
+    // Add images
+    this.svg
+      .selectAll("candidateImages")
+      .data(data)
+      .enter()
+      .append("image")
+      .attr("xlink:href", (d) => d.photo)
+      .attr("x", this.x(0) - this.y.bandwidth())
+      .attr("y", (d) => this.y(d.name))
+      .attr("height", this.y.bandwidth())
+      .attr("width", this.y.bandwidth())
+      .attr("clip-path", "circle()");
+  }
+
+  loadData(dataUrl, filterFunction) {
+    d3.csv(dataUrl).then((rawData) => {
+      let processedData = filterFunction(rawData);
+      this.updateChart(processedData);
+    });
+  }
 }
 
-// Load the data
-d3.csv("data/labeled.csv").then((rawData) => {
-  // data processing code...
-  // Calculate average sentiment label for each candidate
+// Function to filter data
+function filterData(rawData, showAll) {
   const sentimentData = d3
     .rollups(
       rawData,
@@ -86,13 +114,9 @@ d3.csv("data/labeled.csv").then((rawData) => {
         "img/candidate_portraits/" + d[0].split(" ")[1].toLowerCase() + ".png",
       party: rawData.find((r) => r.first_name + " " + r.last_name === d[0])
         .party,
-    }));
+    }))
+    .sort((a, b) => d3.descending(a.avg_sentiment, b.avg_sentiment));
 
-  // Sort data based on avg_sentiment
-  sentimentData.sort((a, b) => d3.descending(a.avg_sentiment, b.avg_sentiment));
-  console.log("Sentiment data:", sentimentData);
-
-  // Calculate the occurrence of each candidate
   const candidateOccurrences = d3
     .rollups(
       rawData,
@@ -101,47 +125,33 @@ d3.csv("data/labeled.csv").then((rawData) => {
     )
     .sort((a, b) => d3.descending(a[1], b[1]));
 
-  // Set dimensions and margins for the chart
-  const margin = { top: 30, right: 30, bottom: 30, left: 230 },
-    width = 800 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-  // Create scales
-  const x = d3
-    .scaleLinear()
-    .domain([0, 1]) // Define the range of average sentiment
-    .range([0, width]);
-
-  const y = d3.scaleBand().range([0, height]).padding(0.1);
-
-  // Append SVG object to the chart div
-  const svg = d3
-    .select("#candidate-sentiment-bars")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
-
-  // Function to filter data
-  function filterData(showAll) {
-    if (showAll) {
-      return sentimentData;
-    } else {
-      const topCandidates = candidateOccurrences.slice(0, 5).map((d) => d[0]);
-      return sentimentData.filter((d) => topCandidates.includes(d.name));
-    }
+  if (showAll) {
+    return sentimentData;
+  } else {
+    const topCandidates = candidateOccurrences.slice(0, 5).map((d) => d[0]);
+    return sentimentData.filter((d) => topCandidates.includes(d.name));
   }
+}
 
-  // Initial chart with top 5 candidates
-  updateChart(filterData(false), svg, x, y, height);
+// Instantiate the chart
+const sentimentChart = new SentimentChart(
+  "#candidate-sentiment-bars",
+  800,
+  500
+);
 
-  // Event listener for the switch
-  d3.select("#candidateToggle").on("change", function () {
-    const showAll = d3.select(this).property("checked");
-    d3.select("#toggleLabel").text(
-      showAll ? "Display All Candidates" : "Show Top 5 Candidates"
-    );
-    updateChart(filterData(showAll), svg, x, y, height);
-  });
+// Load initial data
+sentimentChart.loadData("data/labeled.csv", (rawData) =>
+  filterData(rawData, false)
+);
+
+// Event listener for the toggle switch
+d3.select("#candidateToggle").on("change", function () {
+  const showAll = d3.select(this).property("checked");
+  d3.select("#toggleLabel").text(
+    showAll ? "Display All Candidates" : "Show Top 5 Candidates"
+  );
+  sentimentChart.loadData("data/labeled.csv", (rawData) =>
+    filterData(rawData, showAll)
+  );
 });
