086904062a0276c97927d9488a97723b17310d74
Luke Stoner <lukestoner@g.harvard.edu>
2023-11-30 23:18:19 -0500

    Add network lookup table

---
+++ js/network-lookup.js
@@ -0,0 +1,13 @@
+const networkLookup = {
+    ALJAZ: 'Al Jazeera',
+    BBCNEWS: 'BBC News',
+    CNNW: 'CNN',
+    CSPAN: 'CSPAN',
+    DW: 'DW News (Germany)',
+    FBC: 'Fox Business',
+    FOXNEWSW: 'Fox News',
+    KGO: 'ABC Bay Area',
+    KNTV: 'NBC Bay Area',
+    MSNBCW: 'MSNBC',
+    RT: 'RT News'
+};
