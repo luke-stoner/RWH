bece5653cb38bd1288b07f7a5e4c3002a1ea6c65
kanenorman <kanenorman1996@gmail.com>
2023-12-01 23:13:18 -0500

    Clean up volume

--- js/coverage-volume-bubbles.js
+++ js/coverage-volume-bubbles.js
@@ -7,6 +7,11 @@ class BubbleChart {
     BubbleChart.instance = this;
     this.loadData();
   }
+
+  static getInstance() {
+    return BubbleChart.instance || new BubbleChart();
+  }
+
   async loadData() {
     try {
       const csvData = await d3.csv("data/labeled.csv");
@@ -44,8 +49,8 @@ class BubbleChart {
   }
 
   setupChart(data) {
-    const width = 960;
-    const height = 400;
+    const width = 400;
+    const height = 600;
     const margin = { top: 0, right: 20, bottom: 20, left: 20 };
 
     const innerWidth = width - margin.left - margin.right;
@@ -56,8 +61,7 @@ class BubbleChart {
       .append("svg")
       .attr("width", width)
       .attr("height", height)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .append("g");
 
     const pack = d3.pack().size([innerWidth, innerHeight]).padding(2);
 
@@ -90,18 +94,8 @@ class BubbleChart {
         return i * 50;
       })
       .attr("transform", function (d) {
-        return "translate(" + d.x + "," + d.y + ")";
+        return `translate(${d.x},${d.y})`;
       });
-
-    const sizeScale = d3
-      .scaleSqrt()
-      .domain([
-        0,
-        d3.max(data, function (d) {
-          return d.frequency;
-        }),
-      ])
-      .range([0, 50]);
 
     data.sort(function (a, b) {
       return b.frequency - a.frequency;
@@ -136,67 +130,5 @@ class BubbleChart {
       .attr("width", function (d) {
         return 2 * d.r;
       });
-  }
-
-  setupLegend(data) {
-    const sortedData = data.map((d) => d.frequency).sort((a, b) => a - b);
-    const minSize = this.dynamicRound(sortedData[0]);
-    const medianSize = this.dynamicRound(
-      sortedData[Math.floor(sortedData.length / 2)]
-    );
-    const maxSize = this.dynamicRound(sortedData[sortedData.length - 1]);
-    const legendSizes = [minSize, medianSize, maxSize];
-
-    const sizeScale = d3.scaleSqrt().domain([0, maxSize]).range([5, 30]);
-
-    const spacing = 80;
-    const maxLegendCircleSize = sizeScale(maxSize);
-    const legendWidth =
-      legendSizes.length * (maxLegendCircleSize * 2 + spacing);
-
-    const legendSvg = d3
-      .select("#legend-container")
-      .append("svg")
-      .attr("width", legendWidth)
-      .attr("height", maxLegendCircleSize * 2 + 30);
-
-    const legend = legendSvg
-      .selectAll(".legend")
-      .data(legendSizes)
-      .enter()
-      .append("g")
-      .attr("class", "legend")
-      .attr("transform", function (d, i) {
-        const xPosition = i * (maxLegendCircleSize * 2 + spacing) + 40;
-        return "translate(" + xPosition + ",0)";
-      });
-
-    legend
-      .append("circle")
-      .attr("cx", maxLegendCircleSize)
-      .attr("cy", maxLegendCircleSize)
-      .attr("r", function (d) {
-        return sizeScale(d);
-      })
-      .style("fill", "#ccc");
-
-    legend
-      .append("text")
-      .attr("x", maxLegendCircleSize)
-      .attr("y", maxLegendCircleSize * 2 + 20)
-      .text(function (d) {
-        return `${d.toLocaleString()} mentions`;
-      })
-      .attr("font-size", "12px")
-      .attr("text-anchor", "middle");
-  }
-
-  dynamicRound(value) {
-    if (value === 0) return 0;
-
-    const magnitude = Math.pow(10, Math.floor(Math.log10(Math.abs(value))));
-    const roundTo = magnitude / 10;
-
-    return Math.round(value / roundTo) * roundTo;
   }
 }
