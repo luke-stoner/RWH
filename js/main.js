84fe5330d1bba2880c0ceb5ca70b900ad8a32f21
kanenorman <kanenorman1996@gmail.com>
2023-12-05 20:30:55 -0500

    Refactor

--- js/main.js
+++ js/main.js
@@ -99,9 +99,3 @@ document.addEventListener("DOMContentLoaded", function () {
   handleModalVisibility();
 });
 
-const tooltipTriggerList = document.querySelectorAll(
-  '[data-bs-toggle="tooltip"]'
-);
-const tooltipList = [...tooltipTriggerList].map(
-  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
-);
