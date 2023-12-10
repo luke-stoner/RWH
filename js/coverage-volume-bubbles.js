8dcc041dbddc75759a8e57a672946f3bb15f2915
kanenorman <kanenorman1996@gmail.com>
2023-12-10 18:23:49 -0500

    Add stroke around images

--- js/coverage-volume-bubbles.js
+++ js/coverage-volume-bubbles.js
@@ -51,7 +51,7 @@ class BubbleChart {
     const numCircles = data.length;
 
     // Create SVG container
-    const svg = d3
+    this.svg = d3
       .select("#volume-bubbles")
       .append("svg")
       .attr("width", width)
@@ -78,7 +78,7 @@ class BubbleChart {
     });
 
     // Create circles with radius based on frequency
-    const group = svg.append("g");
+    const group = this.svg.append("g");
 
     const circles = group
       .selectAll("circle")
@@ -178,13 +178,15 @@ class BubbleChart {
 
     // Additional text to show after all transitions are complete
     const totalDelay = numCircles * fanOutDelay;
-    const firstMessage = svg
+    const firstMessage = this.svg
       .append("text")
       .attr("x", width / 2)
       .attr("y", height / 3)
       .text("There is a lot of variance here...")
       .attr("text-anchor", "middle")
       .style("opacity", 0);
+
+    const obj = this;
 
     firstMessage
       .transition()
@@ -198,7 +200,7 @@ class BubbleChart {
       .end() // End first message
       .then(() => {
         // Begin second message
-        const secondMessage = svg
+        const secondMessage = this.svg
           .append("text")
           .attr("x", width / 2)
           .attr("y", height / 3)
@@ -276,7 +278,13 @@ class BubbleChart {
                               .duration(transitionDuration)
                               .style("opacity", 0) // Fade out circles
                               .attr("cx", width / 2) // Move to the middle of the screen horizontally
-                              .attr("cy", height / 2); // Move to the middle of the screen vertically
+                              .attr("cy", height / 2)
+                              .end()
+                              .then(() => {
+                                // End of the first set of animations
+                                obj.svg.selectAll("*").remove();
+                                obj.secondVisualization(data);
+                              });
                           }, pauseDuration);
                         });
                     });
@@ -324,5 +332,100 @@ class BubbleChart {
             focusOnCircle(0);
           });
       });
+  }
+
+  secondVisualization(data) {
+    // Sort data
+    data.sort((a, b) => b.frequency - a.frequency);
+
+    // Get the width and height from the existing SVG
+    const width = +this.svg.attr("width");
+    const height = +this.svg.attr("height");
+    const strokeWidth = 7 / 60;
+
+    const pack = d3.pack().size([width, height]).padding(2);
+
+    // Define a scale for the circle sizes
+    const maxFrequency = d3.max(data, (d) => d.frequency);
+    const radiusScale = d3
+      .scaleSqrt()
+      .domain([0, maxFrequency])
+      .range([10, 100]); // Adjust range as needed
+
+    const root = d3
+      .hierarchy({ children: data })
+      .sum((d) => radiusScale(d.frequency));
+
+    // Create node groups, initially positioned at the center
+    const node = this.svg
+      .selectAll(".node")
+      .data(pack(root).leaves())
+      .enter()
+      .append("g")
+      .attr("class", "node")
+      .attr("transform", `translate(${width / 2},${height / 2})`); // Start at center
+
+    // Create clipPaths for images with initial small radius
+    node
+      .append("clipPath")
+      .attr("id", (d, i) => "clip-" + i)
+      .append("circle")
+      .attr("r", 10); // Start with small radius
+
+    // Append circles to nodes with initial small radius
+    const circles = node
+      .append("circle")
+      .attr("r", 10) // Start with small radius
+      .style("fill", (d) => LIGHT_PARTY_COLOR_MAP[d.data.party])
+      .style("opacity", 0);
+
+    // Append images to nodes with initial small size
+    const images = node
+      .append("svg:image")
+      .attr("xlink:href", (d) => d.data.photo)
+      .attr("clip-path", (d, i) => "url(#clip-" + i + ")")
+      .attr("x", -5) // Half of the initial radius
+      .attr("y", -5) // Half of the initial radius
+      .attr("height", 10) // Double of the initial radius
+      .attr("width", 10) // Double of the initial radius
+      .style("opacity", 0); // Start with opacity 0
+
+    // Transition nodes to their final positions with staggered delay
+    node
+      .transition()
+      .duration(1000) // Duration of the transition in milliseconds
+      .delay((d, i) => i * 100) // Stagger the start of each transition
+      .attr("transform", (d) => `translate(${d.x},${d.y})`);
+
+    // Transition for circles and clipPaths to scale up
+    circles
+      .transition()
+      .duration(1000)
+      .delay((d, i) => i * 100)
+      .attr("r", (d) => radiusScale(d.data.frequency))
+      .style("stroke", (d) => PARTY_COLOR_MAP[d.data.party])
+      .style(
+        "stroke-width",
+        (d) => `${radiusScale(d.data.frequency) * strokeWidth}px`
+      )
+      .style("opacity", 1);
+
+    node
+      .selectAll("clipPath circle")
+      .transition()
+      .duration(1000)
+      .delay((d, i) => i * 100)
+      .attr("r", (d) => radiusScale(d.data.frequency)); // Scale up to final radius
+
+    // Transition for images to scale up and fade in
+    images
+      .transition()
+      .duration(1000)
+      .delay((d, i) => i * 100)
+      .attr("x", (d) => -radiusScale(d.data.frequency))
+      .attr("y", (d) => -radiusScale(d.data.frequency))
+      .attr("height", (d) => 2 * radiusScale(d.data.frequency))
+      .attr("width", (d) => 2 * radiusScale(d.data.frequency))
+      .style("opacity", 1)
   }
 }
