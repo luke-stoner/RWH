2be42f3669d9acddd0e1c3f1237330b87802727d
Luke Stoner <lukestoner@g.harvard.edu>
2023-12-10 19:19:08 -0500

    Fix styling bump-chart

--- js/bump-chart.js
+++ js/bump-chart.js
@@ -44,20 +44,6 @@ d3.csv("data/labeled.csv", row => {
     return row
 }).then(csv => {
 
-    // Set margins, width, height
-    let margin = { top: 40, right: 40, bottom: 70, left: 60 };
-    let width = 900 - margin.left - margin.right;
-    let height = 600 - margin.top - margin.bottom;
-
-    // Initialize SVG drawing space
-    let svg = d3
-        .select("#bump-chart-area")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
     // format percentage
     function formatAsPercentage(num) {
         return new Intl.NumberFormat('default', {
